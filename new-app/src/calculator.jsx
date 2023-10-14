404d5327b790b3e927633d6b6a8e47f5c1c6ba14
ivan kasyanov <koncept.th@gmail.com>
2023-10-14 21:39:07 +0500

    finally

--- new-app/src/calculator.jsx
+++ new-app/src/calculator.jsx
@@ -4,6 +4,7 @@ import styles from './calculator.module.css';
 const calculations = [`+`, `-`, `C`, `=`];
 
 const buttonList = [
+	// все элементы калькулятора в массиве
 	{
 		id: 'one',
 		className: 'calcButtonNumber',
@@ -23,10 +24,10 @@ const buttonList = [
 		func: `default`,
 	},
 	{
-		id: 'close',
+		id: 'clear',
 		className: 'calcButtonSymbol',
 		symbol: 'C',
-		func: `default`,
+		func: `clear`,
 	},
 	{
 		id: 'four',
@@ -96,69 +97,105 @@ const buttonList = [
 	},
 ];
 
-let numberIds = [0, 0];
-let expressionResult = 0;
-let operationNumber = 0;
-let lastParameter = `default`;
-export const CalcMaker = () => {
-	const [expression, setExpression] = useState(``);
+let expressionResult = 0; //результат выражения здесь
+let secondNumber = ``; //второе число
+let writingChanger = false; // true for write in expressionResult(leftNumber);
+let opCounter = 0; //счетчик операций, нужен чтобы вовремя начать считать
+let operationType = `default`; //параметр, содержащий тип операции
 
-	const checkAndWrite = (symbol) => {
+const changerOption = {
+	false: (symbol, expression) => {
+		expressionResult = parseInt(expression + symbol);
+	},
+	true: (symbol) => {
+		secondNumber += symbol;
+	},
+};
+
+export const CalcMaker = () => {
+	const [expression, setExpression] = useState(` `); //выражение на экране калькулятора
+
+	const types = {
+		// сборник функций для кнопок меню
+		plus: (leftNumber, rightNumber) => {
+			return leftNumber + rightNumber;
+		},
+		minus: (leftNumber, rightNumber) => {
+			return leftNumber - rightNumber;
+		},
+		equal: () => {
+			setExpression(expression + `=` + expressionResult.toString());
+		},
+		clear: () => {
+			window.location.reload(); // я надеюсь так можно было
+		},
+	};
+
+	const checkAndWrite = (symbol, func) => {
+		//проверка какой символ введен, цифра или нет
 		if (
 			!calculations.includes(symbol) ||
 			(!(expression[expression.length - 1] === symbol) &&
 				!calculations.includes(expression[expression.length - 1]))
 		) {
-			setExpression(expression + symbol);
+			if (!calculations.includes(symbol)) {
+				if (operationType !== `equal`) {
+					changerOption[writingChanger](symbol, expression);
+					setExpression(expression + symbol);
+				}
+			} else {
+				document.querySelector(`#screen`).style.color = `white`;
+				if (func !== `equal`) {
+					if (!writingChanger) {
+						writingChanger = !writingChanger;
+					}
+					operationType = func;
+				}
+				opCounter++;
+				setExpression(expression + symbol);
+			}
 		} else {
 			setExpression(expression.replace(expression[expression.length - 1], symbol));
+			operationType = func;
 		}
+		console.log(symbol);
+		console.log(`expression = `, expression);
+		console.log(`expressionResult = `, expressionResult);
+		console.log(`secondNumber = `, secondNumber);
+		console.log(`operationType = `, operationType);
+		console.log(`writingChanger = `, writingChanger);
+		console.log(`changeCounter = `, opCounter);
+		console.log(`-----------------------------------------------`);
 	};
 
-	const resultCalc = (parameter) => {
-		switch (parameter) {
-			case `plus`:
-				numberIds[0] = numberIds[1];
-				numberIds[1] = expression.length;
-				operationNumber = parseInt(
-					expression.substring(numberIds[0], numberIds[1]),
-				);
-				expressionResult += operationNumber;
-				console.log(expressionResult);
-				console.log(numberIds);
-				break;
-			case `minus`:
-				numberIds[0] = numberIds[1];
-				numberIds[1] = expression.length;
-				operationNumber = parseInt(
-					expression.substring(numberIds[0], numberIds[1]),
-				);
-				expressionResult -= operationNumber;
-				console.log(expressionResult);
-				console.log(numberIds);
-				break;
-			case `equal`:
-				resultCalc(lastParameter);
-				setExpression(expression + expressionResult);
-				break;
-			default:
-				console.log(`number`);
+	const setСalculations = (symbol, func) => {
+		//главная функция
+		checkAndWrite(symbol, func);
+		if (opCounter > 1) {
+			expressionResult = types[operationType](
+				expressionResult,
+				parseInt(secondNumber),
+			);
+			opCounter--;
+			secondNumber = ``;
+		}
+		if (func === `equal`) {
+			opCounter = 0;
+			operationType = `equal`;
+			types.equal();
+			document.querySelector(`#screen`).style.color = `red`;
+		}
+		if (func === `clear`) {
+			types.clear();
 		}
 	};
-
-	async function setСalculations(symbol, parameter) {
-		await checkAndWrite(symbol);
-		if (parameter === `plus` || parameter === `minus`) {
-			lastParameter = parameter;
-		}
-		operationNumber = parseInt(expression.substring(numberIds[0], numberIds[1]));
-		//resultCalc(parameter);
-	}
 
 	return (
 		<>
 			<div className={styles.calcWindow}>
-				<div className={styles.calcScreenExpression}>{expression}</div>
+				<div id="screen" className={styles.calcScreenExpression}>
+					{expression}
+				</div>
 				{buttonList.map(({ id, className, symbol, func }) => {
 					return (
 						<button
